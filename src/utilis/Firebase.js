623396d3f13ae4a78255430db956a1aae2364748
Mariano Sanchez Hernandez <marianosanchez0307@gmail.com>
2024-05-14 00:25:55 -0400

    changed title

--- src/utilis/Firebase.js
+++ src/utilis/Firebase.js
@@ -6,12 +6,12 @@ import { getFirestore, doc, getDoc, setDoc, collection, writeBatch, query, getDo
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDUwSQ8RlDzcwPY5Eq2wdmpLKRHqSw9NHw",
-    authDomain: "plm-fleet.firebaseapp.com",
-    projectId: "plm-fleet",
-    storageBucket: "plm-fleet.appspot.com",
-    messagingSenderId: "581262701095",
-    appId: "1:581262701095:web:9cebef65a6c3d3f6f45402"
+    apiKey: "AIzaSyBTINJyBV3sDQJp9xLMVt8VdU-BkvrB4Gk",
+    authDomain: "plm-starbucks.firebaseapp.com",
+    projectId: "plm-starbucks",
+    storageBucket: "plm-starbucks.appspot.com",
+    messagingSenderId: "788196609197",
+    appId: "1:788196609197:web:f0800198a84e80f728ca24"
   };
   
 
